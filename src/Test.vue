14ac795fc42cf0a10f7ecf8d7c70c1d22a2b1092
Mike Faber <mfaber@assurity.com>
2020-01-17 13:15:31 -0600

    Vue init

---
+++ src/Test.vue
@@ -0,0 +1,20 @@
+<template>
+    <p id="text">{{ text }}</p>
+</template>
+
+<script>
+export default {
+    name: 'text',
+    data () {
+        return {
+            text: "Hello World!"
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .text {
+        color: blue
+    }
+</style>
