ebaa9d8fd3c6d6754740ee0f593e53259cb0cec1
Mike Faber <mfaber@assurity.com>
2020-01-20 10:48:02 -0600

    Hero component working, removing Test component

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,54 @@
 <template>
   <div id="app">
+    <Hero image="src\assets\clouds.jpeg" alt="Landscape with sun and clouds" />
+
     <h1 id="title">{{ title }}</h1>
-    <div id="body">{{ body }}</div>
-    <br />
-    <Test text="Hello World!"/>
+    <div id="text">{{ text }}</div>    
   </div>
 </template>
 
 <script>
-import Test from './Test'
+import Hero from "./Hero"
 
 export default {
   name: 'app',
   components: {
-    Test
+    Hero
   },
   data () {
     return {
       title: 'POC',
-      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     }
   }
 }
 </script>
 
 <style lang="scss">
+
+html, body {
+  margin: 0px !important;
+  padding: 0px !important;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0 auto;
+  padding: 0px;
 }
 
-#body {
+#text {
   margin: 0 auto;
+  padding: 0px;
   width: 50%;
 }
 
-h1, h2 {
+h1 {
   font-weight: normal;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
---
+++ src/Hero.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="hero">
+        <img id="image" :src="image" :alt="alt">
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['image', 'alt']
+}
+</script>
+
+<style scoped lang="scss">
+
+#hero {
+    width: 100%;
+    margin: 0 auto;
+    padding: 0px;
+    align-content: center;
+}
+
+#image {
+    width: 100%;
+    height: 600px;
+    object-fit: cover;
+}
+
+</style>
