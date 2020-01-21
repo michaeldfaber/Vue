5236a09943e7edf87a4d6f5a95f209e686790226
Mike Faber <mfaber@assurity.com>
2020-01-21 10:57:54 -0600

    Routing working with Navbar component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
-import TestPage from './components/TestPage.vue'
 import VueRouter from 'vue-router'
+import App from './App.vue'
+import Home from './components/Home.vue'
+import TestPage from './components/TestPage.vue'
 
 Vue.use(VueRouter)
 
@@ -9,20 +10,13 @@ const router = new VueRouter({
   mode: 'history',
   base: __dirname,
   routes: [
-    { path: '/', component: App },
+    { path: '/', component: Home },
     { path: '/test', component: TestPage }
   ]
 })
 
 new Vue({
   router,
-  template: `
-    <div id="app">
-      <router-link class="navbar-link" to="/">Home</router-link>
-      <router-link class="navbar-link" to="/test">Test Page</router-link>
-      <router-view></router-view>
-    </div>
-  `,
-  el: '#app'
-  // render: h => h(App)
+  el: '#app',
+  render: h => h(App)
 })
